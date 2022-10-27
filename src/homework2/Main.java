8d8bbf64b17db6b8fe775dc68c797ec688e166f6
Andrey Polezhaiev <hillelpolezhaiev@gmail.com>
2022-10-27 13:24:53 +0300

    New branch feature/some-feature

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -3,5 +3,6 @@ package homework2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Second homework of Java Pro");
+        System.out.println("New branch feature/some-feature");
     }
 }
