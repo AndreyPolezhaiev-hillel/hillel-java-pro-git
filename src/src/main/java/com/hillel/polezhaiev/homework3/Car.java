02dece382fc31282cdb6a19a4b5987090f48ecc9
Andrey Polezhaiev <hillelpolezhaiev@gmail.com>
2022-10-31 13:50:33 +0200

    HOMEWORK 3

---
+++ src/src/main/java/com/hillel/polezhaiev/homework3/Car.java
@@ -0,0 +1,23 @@
+package com.hillel.polezhaiev.homework3;
+
+    public class Car{
+
+        public void start(){
+            startElectricity();
+            startCommand();
+            startFuelSystem();
+        }
+        private void startElectricity(){
+            System.out.println("Electricity has been started");
+        }
+
+        private void startCommand(){
+            System.out.println("Command has been started");
+        }
+
+        private void startFuelSystem(){
+            System.out.println("Fuel System has been started");
+        }
+
+    }
+
